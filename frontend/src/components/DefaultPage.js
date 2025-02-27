416fc9a06f36b2d875197825c653faabd36861cb
Myshae <myshaeallen@outlook.com>
2025-02-27 10:36:23 -0600

    creating default page

--- frontend/src/components/DefaultPage.js
+++ frontend/src/components/DefaultPage.js
@@ -1,5 +1,19 @@
+import { use, useEffect} from "react";
 
-export default function AccountPage() {
+export default function DefaultPage() {
+    useEffect(() => {
+        // Fetch the users from the API
+        fetch(`https://miso-eight.vercel.app/users`)
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+        })
+        .catch((error) => {
+        console.error("'use effect FAILED TO FETCH: ", error);
+        });
+    }, []);
     return (
         <div>
             <h1>Default Page</h1>
